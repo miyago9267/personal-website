38bfc5766fbd20d08edc6a3c3ec4e7472459afaf
miyago9267 <miyagomizugen@gmail.com>
2023-12-20 03:52:28 +0800

    feat: init comp and create todo

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+    <header>
+        <nav>
+            <div>
+                <!-- icon -->
+            </div>
+            <div>
+                <!-- menu-->
+            </div>
+            <div>
+                <!-- menu with hamburger -->
+            </div>
+        </nav>
+    </header>
+</template>
