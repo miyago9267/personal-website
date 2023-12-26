e56503f6dac89131f2a1d04f39fdbc30f902255f
miyago9267 <miyagomizugen@gmail.com>
2023-12-26 18:59:46 +0800

    feat: change title font size

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@ defineProps<{ msg: string }>()
 <template>
   <div class="flex flex-col w-full h-screen justify-center">
     <div class="flex flex-col ">
-      <p class="nerd md:text-6xl text-xl mb-2">Hello World, Miyago here</p>
+      <p class="nerd md:text-7xl text-xl mb-2">Hello World, Miyago here</p>
       <p class="nerd md:text-2xl text-lg">MUGer x Composer x Programmer</p>
     </div>
   </div>
